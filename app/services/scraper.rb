9f90c3271fd8f78371aa5d788835fd483fa4a73b
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-02-17 13:55:27 +0530

    This is my forth commit after writing services for fetching data returned by server

---
+++ app/services/scraper.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'httparty'
+
+class Scraper
+ def self.scarpe(url)
+    response= Httparty.get(url)
+    doc = Nokogiri::HTML(response.body)
+    {
+      title: doc.at_css("span.B_NuCI")&.text.strip,
+      price: doc.at_css("div._30jeq3")&.text.strip,
+      description: doc.at_css("div._1mXcCf")&.text.strip,
+      category: doc.at_css("a._2whKao")&.text.strip
+    }
+ end
+end
